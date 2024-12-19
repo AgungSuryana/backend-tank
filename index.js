c140b24a523c8d17a9154a350fce2d6c72208022
DavinArisandi <agungsur84@gmail.com>
2024-12-19 18:38:31 +0700

    cors

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const connectToDatabase = require('./config/connectdb');
 const dht22Routes = require('./routes/dht22');
 const piezzoRoutes = require('./routes/piezzo');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+
+app.use(cors());
+
+// Jika ingin lebih spesifik:
+app.use(
+    cors({
+        origin: 'https://rc-tank-f4.vercel.app', // Ganti dengan domain frontend Anda
+        methods: ['GET', 'POST'],
+        credentials: true, // Jika perlu cookie atau header tertentu
+    })
+);
 
 // Connect to database
 connectToDatabase();
